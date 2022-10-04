53664b7a610c9ad12ee1d86fbaa302fcad7d1346
Brian Fletcher <brian@roadie.io>
2022-10-04 14:54:30 +0100

    fix: copy mdi logos to reduce the size of backstage bundle

---
+++ src/components/SnykEntityComponent/svgs/mdiLambda.ts
@@ -0,0 +1,2 @@
+// The following was copied from https://github.com/Templarian/MaterialDesign-JS/blob/3c2f1225ad685a16ecfe818e612b40ee80541306/mdi.js to improve the performance of the backstage application.
+export var mdiLambda = "M6,20L10.16,7.91L9.34,6H8V4H10C10.42,4 10.78,4.26 10.93,4.63L16.66,18H18V20H16C15.57,20 15.21,19.73 15.07,19.36L11.33,10.65L8.12,20H6Z";
---
+++ src/components/SnykEntityComponent/svgs/mdiMicrosoftAzureDevops.ts
@@ -0,0 +1,2 @@
+// The following was copied from https://github.com/Templarian/MaterialDesign-JS/blob/3c2f1225ad685a16ecfe818e612b40ee80541306/mdi.js to improve the performance of the backstage application.
+export var mdiMicrosoftAzureDevops = "M22 18L17 22L9 19V22L4.81 16.25L17.72 17.3V6.34L22 5.65V18M4.81 16.25V8.96L17.72 6.34L10.6 2V4.84L3.97 6.76L2 9.38V15.07L4.81 16.25Z";
