ab9839dfe1456c117e5035945f3873877ba274c8
Antoine Arlaud <antoine@snyk.io>
2023-06-26 17:11:54 +0200

    feat: use v3 projects issues endpoints+ref by targets+update ui compnts
    BREAKING CHANGE: The project IDs annotations requirement is no longer applicable, being replaced by the target. Target is identified by the repo name in the form of project-slug for github, or specific target id retrievable by API call"

--- src/components/SnykEntityComponent/snykTab.tsx
+++ src/components/SnykEntityComponent/snykTab.tsx
@@ -12,41 +12,36 @@ import { SnykApi } from "../../api/index";
 import { useAsync } from "react-use";
 import { Alert } from "@material-ui/lab";
 import { IssuesTable } from "./components/SnykIssuesComponent";
-import { LicenseIssuesTable } from "./components/SnykLicenseIssuesComponent";
-import { IgnoredIssuesTable } from "./components/SnykIgnoredIssuesComponent";
 import { DepGraphInfo } from "./components/SnykDepGraphComponent";
 import { SnykCircularCounter } from "./components/SnykCircularCountersComponent";
 import * as utils from "../../utils/utils";
 import {
-  IssuesArray,
   ProjectGetResponseType,
   DepgraphGetResponseType,
 } from "../../types/types";
+import { Issue, TypeDef, UnifiedIssues } from "../../types/unifiedIssuesTypes";
 
 
 export const generateSnykTabForProject = (
   snykApi: SnykApi,
-  orgName: string,
-  projectId: string
+  orgId: string,
+  projectId: string,
+  orgDisplayName?: string
 ) => {
+  const genericIssuesTypeArray = Object.values(TypeDef).filter(type => type != 'license')
   return ({}) => {
     const { value, loading, error } = useAsync(async () => {
-      const vulnsIssues: IssuesArray = await snykApi.ListAllAggregatedIssues(
-        orgName,
-        projectId
-      );
-      const licenseIssues: IssuesArray =
-        await snykApi.ListAllAggregatedLicenseIssues(orgName, projectId);
-      const ignoredIssues: IssuesArray = await snykApi.ListAllIgnoredIssues(
-        orgName,
-        projectId
-      );
-      const depGraph: DepgraphGetResponseType =
-        await snykApi.GetDependencyGraph(orgName, projectId);
+      const allIssues: UnifiedIssues = await snykApi.ListAllAggregatedIssues(orgId,projectId)
+      const genericIssues: Array<Issue> = allIssues.data.filter(issue => genericIssuesTypeArray.includes(issue.attributes.type))
+      const licenseIssues: Array<Issue> = allIssues.data.filter(issue => issue.attributes.type == 'license')
+      const ignoredIssues: Array<Issue> = allIssues.data.filter(issue => issue.attributes.ignored == true)
+
+      const depGraph: DepgraphGetResponseType = genericIssues.some(issue => issue.attributes.type == TypeDef.PackageVulnerability) ? 
+        await snykApi.GetDependencyGraph(orgId, projectId): null;
       const projectDetails: ProjectGetResponseType =
-        await snykApi.ProjectDetails(orgName, projectId);
+        await snykApi.ProjectDetails(orgId, projectId);
       return {
-        vulnsIssues,
+        genericIssues,
         licenseIssues,
         ignoredIssues,
         depGraph,
@@ -67,75 +62,126 @@ export const generateSnykTabForProject = (
       return <Alert severity="error">Empty response</Alert>;
     }
 
-    const issuesCount = utils.getIssuesCount(value.vulnsIssues);
+    const issuesCount = utils.getIssuesCount(value.genericIssues);
     const licenseIssuesCount = utils.getIssuesCount(value.licenseIssues);
     const ignoredIssuesCount = utils.getIssuesCount(value.ignoredIssues);
-    //const cardContentStyle = { height: 200, width: 500 };
+    
 
     const metadata = {
       origin: value.projectDetails.origin,
       type: value.projectDetails.type,
       "last tested": value.projectDetails.lastTestedDate,
-      repository: value.projectDetails.remoteRepoUrl,
+      repository: value.projectDetails.remoteRepoUrl || orgDisplayName,
     };
     const linkInfo = {
       title: "More details",
-      link: `https://${snykApi.GetSnykAppHost()}/org/${orgName}/project/${projectId}`,
+      link: `https://${snykApi.GetSnykAppHost()}/org/${orgDisplayName}/project/${projectId}`,
     };
-
-    return (
-      <Grid style={{ marginTop: "2px" }} container spacing={2}>
-        <Grid item xs={12}>
-          <Grid container spacing={2} justify="space-between" direction="row">
-            <Grid item xs={12}>
-              <InfoCard title={value.projectDetails.name}>
-                <StructuredMetadataTable metadata={metadata} />
-              </InfoCard>
-            </Grid>
-          </Grid>
-          <Grid container spacing={2} justify="space-between" direction="row">
-            <Grid item xs={3}>
-              <InfoCard title="Vulnerabilities">
-                <SnykCircularCounter issuesCount={issuesCount} />
-              </InfoCard>
-            </Grid>
-            <Grid item xs={3}>
-              <InfoCard title="License Issues">
-                <SnykCircularCounter issuesCount={licenseIssuesCount} />
-              </InfoCard>
-            </Grid>
-            <Grid item xs={3}>
-              <InfoCard title="Ignored Issues">
-                <SnykCircularCounter issuesCount={ignoredIssuesCount} />
-              </InfoCard>
-            </Grid>
-          </Grid>
-
-        <Grid container spacing={2} justify="space-between" direction="row">
+    if(value.depGraph){
+      return (
+        <>
+         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <TabbedCard deepLink={linkInfo}>
-              <CardTab label="Issues">
-                <IssuesTable issues={value.vulnsIssues.issues} pageUrl={linkInfo.link} />
-              </CardTab>
-              <CardTab label="License Issues">
-                <LicenseIssuesTable issues={value.licenseIssues.issues} pageUrl={linkInfo.link} />
-              </CardTab>
-              <CardTab label="Dependencies">
-                <Grid container>
-                  <DepGraphInfo depGraph={value.depGraph} />
-                </Grid>
-              </CardTab>
-              <CardTab label="Ignored">
-                <IgnoredIssuesTable issues={value.ignoredIssues.issues} />
-              </CardTab>
-              {/* <CardTab label="Trends">
-                    <div style={cardContentStyle}>Some content 4</div>
-                  </CardTab> */}
-            </TabbedCard>
+            <Grid container spacing={2} justifyContent="space-between" direction="row">
+              <Grid item xs={12}>
+                <InfoCard title={value.projectDetails.name}>
+                  <StructuredMetadataTable metadata={metadata} />
+                </InfoCard>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} justifyContent="space-between" direction="row">
+              <Grid item xs={3}>
+                <InfoCard title="Vulnerabilities">
+                  <SnykCircularCounter issuesCount={issuesCount} />
+                </InfoCard>
+              </Grid>
+              <Grid item xs={3}>
+                <InfoCard title="License Issues">
+                  <SnykCircularCounter issuesCount={licenseIssuesCount} />
+                </InfoCard>
+              </Grid>
+              <Grid item xs={3}>
+                <InfoCard title="Ignored Issues">
+                  <SnykCircularCounter issuesCount={ignoredIssuesCount} />
+                </InfoCard>
+              </Grid>
+            </Grid>
+  
+          <Grid container spacing={2} justifyContent="space-between" direction="row">
+            <Grid item xs={12}>
+              <TabbedCard deepLink={linkInfo}>
+                <CardTab label="Issues">
+                  <IssuesTable issues={value.genericIssues} pageUrl={linkInfo.link} />
+                </CardTab>
+                <CardTab label="License Issues">
+                  <IssuesTable issues={value.licenseIssues} pageUrl={linkInfo.link} />
+                </CardTab>
+               <CardTab label="Dependencies" >
+                  <Grid container>
+                    <DepGraphInfo depGraph={value.depGraph} />
+                  </Grid>
+                </CardTab>
+                <CardTab label="Ignored">
+                  <IssuesTable issues={value.ignoredIssues} pageUrl={linkInfo.link}/>
+                </CardTab>
+                
+              </TabbedCard>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Grid>
-    );
+      </Grid>          
+      </> 
+      );
+    } else {
+      return (
+        <>
+         <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Grid container spacing={2} justifyContent="space-between" direction="row">
+              <Grid item xs={12}>
+                <InfoCard title={value.projectDetails.name}>
+                  <StructuredMetadataTable metadata={metadata} />
+                </InfoCard>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} justifyContent="space-between" direction="row">
+              <Grid item xs={3}>
+                <InfoCard title="Vulnerabilities">
+                  <SnykCircularCounter issuesCount={issuesCount} />
+                </InfoCard>
+              </Grid>
+              <Grid item xs={3}>
+                <InfoCard title="License Issues">
+                  <SnykCircularCounter issuesCount={licenseIssuesCount} />
+                </InfoCard>
+              </Grid>
+              <Grid item xs={3}>
+                <InfoCard title="Ignored Issues">
+                  <SnykCircularCounter issuesCount={ignoredIssuesCount} />
+                </InfoCard>
+              </Grid>
+            </Grid>
+  
+          <Grid container spacing={2} justifyContent="space-between" direction="row">
+            <Grid item xs={12}>
+              <TabbedCard deepLink={linkInfo}>
+                <CardTab label="Issues">
+                  <IssuesTable issues={value.genericIssues} pageUrl={linkInfo.link} />
+                </CardTab>
+                <CardTab label="License Issues">
+                  <IssuesTable issues={value.licenseIssues} pageUrl={linkInfo.link} />
+                </CardTab>
+                <CardTab label="Ignored">
+                  <IssuesTable issues={value.ignoredIssues} pageUrl={linkInfo.link}/>
+                </CardTab>
+                
+              </TabbedCard>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>           
+      </>   
+      );
+    }
   };
 };
--- yarn-links.sh
+++ yarn-links.sh
@@ -3,6 +3,6 @@
 # for development only
 
 yarn link react
-yarn link react-use
 yarn link react-router-dom
-yarn link @backstage/plugin-catalog-react
+yarn link react-dom
+yarn link react-router
