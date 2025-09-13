93f1d2be633bb13ac9414b851d71397d89a77408
Camila Belo <camilaibs@gmail.com>
2025-09-13 12:57:21 +0200

    refactor: apply review suggestions
    Signed-off-by: Camila Belo <camilaibs@gmail.com>

--- src/alpha/entityContents.tsx
+++ src/alpha/entityContents.tsx
@@ -4,9 +4,8 @@ import {
   convertLegacyRouteRef,
 } from "@backstage/core-compat-api";
 import { EntityContentBlueprint } from "@backstage/plugin-catalog-react/alpha";
-
 import { entityContentRouteRef } from "../routes";
-import { SNYK_ANNOTATION_ORG, SNYK_ANNOTATION_ORGS, SNYK_ANNOTATION_PROJECTIDS, SNYK_ANNOTATION_TARGETID, SNYK_ANNOTATION_TARGETNAME, SNYK_ANNOTATION_TARGETS } from "../config";
+import { isSnykAvailableEntityPredicate } from "./entityPredicates";
 
 /** @alpha */
 export const snykEntityContent = EntityContentBlueprint.make({
@@ -14,48 +13,7 @@ export const snykEntityContent = EntityContentBlueprint.make({
     path: "/snyk",
     title: "Snyk",
     routeRef: convertLegacyRouteRef(entityContentRouteRef),
-    filter: {
-      $all: [
-        {
-          $any: [
-            {
-              [`metadata.annotations.${SNYK_ANNOTATION_ORG}`]: {
-                $exists: true,
-              },
-            },
-            {
-              [`metadata.annotations.${SNYK_ANNOTATION_ORGS}`]: {
-                $exists: true,
-              },
-            },
-          ],
-        },
-        {
-          $any: [
-            {
-              [`metadata.annotations.${SNYK_ANNOTATION_TARGETS}`]: {
-                $exists: true,
-              },
-            },
-            {
-              [`metadata.annotations.${SNYK_ANNOTATION_TARGETID}`]: {
-                $exists: true,
-              },
-            },
-            {
-              [`metadata.annotations.${SNYK_ANNOTATION_TARGETNAME}`]: {
-                $exists: true,
-              },
-            },
-            {
-              [`metadata.annotations.${SNYK_ANNOTATION_PROJECTIDS}`]: {
-                $exists: true,
-              },
-            },
-          ],
-        },
-      ],
-    },
+    filter: isSnykAvailableEntityPredicate,
     async loader() {
       const { SnykEntityComponent } = await import(
         "../components/SnykEntityComponent"
