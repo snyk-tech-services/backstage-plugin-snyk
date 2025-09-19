404e06e041628051a6b348c291a4bf5932aefc48
Danylo Hotvianskyi <danilgotvyansky@gmail.com>
2025-09-19 18:51:56 +0200

    snyk-tech-services/backstage-plugin-snyk#209 prettier fixes
    Signed-off-by: Danylo Hotvianskyi <danilgotvyansky@gmail.com>

--- src/components/SnykEntityComponent/index.ts
+++ src/components/SnykEntityComponent/index.ts
@@ -1,4 +1,4 @@
-import {Entity} from "@backstage/catalog-model";
+import { Entity } from '@backstage/catalog-model';
 import {
   SNYK_ANNOTATION_ORG,
   SNYK_ANNOTATION_ORGS,
@@ -7,10 +7,10 @@ import {
   SNYK_ANNOTATION_TARGETDISPLAY,
   SNYK_ANNOTATION_TARGETS,
   SNYK_ANNOTATION_PROJECTIDS,
-} from "../../config";
+} from '../../config';
 
-export {SnykEntityComponent} from "./SnykEntityComponent";
-export {SnykOverview, SnykOverviewTable} from "./SnykOverviewComponent";
+export { SnykEntityComponent } from './SnykEntityComponent';
+export { SnykOverview, SnykOverviewTable } from './SnykOverviewComponent';
 
 export const isPluginApplicableToEntity = (entity: Entity) =>
   (Boolean(entity.metadata.annotations?.[SNYK_ANNOTATION_ORG]) ||
